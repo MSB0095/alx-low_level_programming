28a798b4124272637b5815b1d7bef61f3cf9cc86
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-16 18:00:21 -0700

    with proper grammar, but the outcome is a piece of art,

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 
 /**
- * main - Entry point (only prints 'with proper grammar, but the outcome is a piece of art,')
+ * main - Entry point
+ * (only prints 'with proper grammar, but the outcome is a piece of art,')
  * Return: Always 0 (Success)
  */
 
