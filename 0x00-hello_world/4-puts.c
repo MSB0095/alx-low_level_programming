56cc22dbbc8bab180a7c30345f9c4ff0e81ff452
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-16 17:48:29 -0700

    Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -2,11 +2,10 @@
 
 /*
  * main - Entry point
- * return - 0
+ * Return - Always 0 (Success)
  */
-
 int main()
 {
-	puts("Programming is like building a multilingual puzzle");
+	puts("Programming is like building a multilingual puzzle\n");
 	return (0);
 }
