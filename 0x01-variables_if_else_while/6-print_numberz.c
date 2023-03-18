e5994b4e05fe078a7891ed4749d46f67e87d0723
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-17 17:41:05 -0700

    numberz

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+		putchar(48 + n);
+	putchar('\n');
+	return (0);
+}
+
+
