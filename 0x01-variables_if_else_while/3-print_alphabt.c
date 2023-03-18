c977c0e53f3e0622858d970571ba7d2df639756a
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-17 17:21:34 -0700

    alphabets except q and e

---
+++ 0x01-variables_if_else_while/3-print_alphabt.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		if (ch = 'q' || ch = 'e')
+			continue;
+		else
+			putchar(ch);
+	putchar(\n);
+	return (0);
+}
