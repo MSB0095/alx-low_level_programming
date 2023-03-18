ea8d69b7f5484444aa3a993c017203706a845f44
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-17 17:56:42 -0700

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 9; n++)
+	{
+		if (n == 9)
+			putchar(48 + n);
+			break;
+		putchar(48 + n);
+		putchar(',');
+		putchar(' ');
+	}
+	return (0);
+}
+
+
