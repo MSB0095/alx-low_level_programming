d4e4e738b841b66f3f26b2de306f2e9d0e3e800d
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-17 17:47:43 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int ch;
+
+	for (ch = 0; ch <= 9; ch++)
+		putchar(48 + n);
+	for (ch = 'a'; ch <= 'f'; ch++)
+		putchar(ch);
+	putchar('\n');
+	return (0);
+}
+
