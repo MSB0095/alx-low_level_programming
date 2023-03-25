889bae0fec30f4c41d7b0c9e96937f2906424e52
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-24 17:18:06 -0700

    isupper

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _isupper - Checks if a character is upper or lower case
+ * Return: 1 if uppercase and 0 otherwise
+ **/
+
+int _isupper(int c)
+{
+	if ( c >= 'A' && c <= 'Z' )
+		return (1);
+	else
+		return (0);
+}
