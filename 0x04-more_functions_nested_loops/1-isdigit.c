66a58f86e96189ba7763d9671318e0549ccb8c7c
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-24 17:43:04 -0700

    isdigit

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,10 @@
+#include "main.h"
+
+/** _isdigit - checks if a character a digit
+ * @c: character to be checked
+ * Return: 1 if digit 0 if not
+ * */
+int _isdigit(int c)
+{
+	return (c >= '0' && c <= '9')
+}
