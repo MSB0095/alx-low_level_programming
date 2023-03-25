5111dbac8b9e8ee0ade5f7e0adf671a68d5e3198
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-24 17:26:04 -0700

    isupper

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,9 +2,9 @@
 
 /**
  * _isupper - Checks if a character is upper or lower case
+ * @c: the character to be checked
  * Return: 1 if uppercase and 0 otherwise
  **/
-
 int _isupper(int c)
 {
 	return (c >= 'A' && c <= 'Z');
