7d7f15dd68353e4d306bd25e5caee51f676bc844
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-24 17:57:20 -0700

    printnums

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints numbers followed with new line
+ * Return: 0
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0,i < 10,i++)
+		_putchar(i);
+	_putchar('\n');
+}
