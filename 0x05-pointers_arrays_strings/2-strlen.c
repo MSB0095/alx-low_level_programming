4aee0e3031d3c30699c074636a99e846d405fe52
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-28 18:25:17 -0700

    _strlen

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * _strlen - a function that returns the length of a string.
+ * @s: the string pointer
+ */
+
+int _strlen(char *s)
+{
+	int len;
+
+	len = strlen(*s);
+	return (len);
+}
