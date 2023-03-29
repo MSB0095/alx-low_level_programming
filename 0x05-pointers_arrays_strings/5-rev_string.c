eb2a4977fc22fa825fec27f5f0437494f437e8da
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-28 19:32:05 -0700

    rev_string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * rev_string - reverses a string
+ * @s: the string
+ */
+
+void rev_string(char *s)
+{
+	int a, b, len;
+	
+	a = 0;
+
+	while (s[a] =! '\0')
+	{
+		a++;
+	}
+
+	len = a;
+
+	char t[len];
+	
+	for (a = len - 1, a >=0, a--)
+	{
+		for (b = 0, b <= len, b++)
+		{
+			t[b] = s[a];
+		}
+	}
+
+	for (b = 0, b <= len, b++)
+	{
+		s[b] = t[b];
+	}
+}
