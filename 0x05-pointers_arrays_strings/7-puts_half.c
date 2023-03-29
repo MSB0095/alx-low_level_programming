ee712e9087274d60f635438b05e18cc30a8cb9d2
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-28 19:52:12 -0700

    fix

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -20,15 +20,15 @@ void puts_half(char *str)
 	{
 		for (i = len / 2; str[i] != '\0'; i++)
 		{
-			_putchar(str[i]);
+			putchar(str[i]);
 		}
 	} else if (len % 2)
 	{
 		for (n = (len - 1) / 2; n < len - 1; n++)
 		{
-			_putchar(str[n + 1]);
+			putchar(str[n + 1]);
 		}
 	}
-	_putchar('\n');
+	putchar('\n');
 }
 
