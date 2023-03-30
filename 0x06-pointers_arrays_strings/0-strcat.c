0f89cd362960c2186fbdecb447c603e636abaacb
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-30 16:28:26 -0700

    fix

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -7,24 +7,9 @@
  * Return: *dest
  */
 
-char *_strcat(char *dest, char *src)
+char _strcat(char *dest, char *src)
 {
-	int lensrc, i, j, lendest, len;
-
-	lendest = strlen(dest);
-	lensrc = strlen(src);
-	len = lensrc + lendest - 2;
-	i = lendest - 1;
-	j = 0;
-	for (i < len;i++;)
-	{
-		while(j <= lensrc)
-		{
-			dest[i] = src[j];
-			j++;
-		}
-	}
-	dest[len] = '\0';
-	return (dest);
+	char *str;
+	str = strcat(dest, src);
+	return(str);
 }
-
--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -1,4 +1,4 @@
 #include <stdio.h>
 #include <string.h>
 
-char *_strcat(char *dest, char *src);
+char _strcat(char *dest, char *src);
