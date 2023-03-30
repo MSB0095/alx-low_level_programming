96735e1f3bb2a6d5035aac4c1680119716c11d0d
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-03-30 15:45:45 -0700

    concatenate

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -14,7 +14,7 @@ char *_strcat(char *dest, char *src)
 	lendest = strlen(dest);
 	lensrc = strlen(src);
 	len = lensrc + lendest - 2;
-	i = lensrc - 1;
+	i = lendest - 1;
 	j = 0;
 	for (i < len;i++)
 	{
