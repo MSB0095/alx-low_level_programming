7b9fefc3b66397e2111906dd4dc77457a538a6bb
Mohamed Salah Belbsir <mohamedsalah.belbsir2@gmail.com>
2023-04-04 19:23:38 -0700

    second task

--- 0x07-pointers_arrays_strings/1-memcpy.c
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -1,22 +1,26 @@
 #include "main.h"
 
 /**
- * *_memcpy - copies n bytes from memory area src to memory area dest
- * @dest: destination
- * @src: source
- * @n: number of bytes to copy
- * Return: a pointer to dest
+ * _memcpy - function copies @n bytes from memory area @src
+ * to memory area @dest
+ *
+ * @n: function copies
+ *
+ * @src: bytes from memory area
+ *
+ * @dest: to memory area
+ *
+ * Return: a pointer to @dest
  */
-
 char *_memcpy(char *dest, char *src, unsigned int n)
 {
-	unsigned int i;
+	unsigned int i = 0;
 
-	i = 0;
 	while (i < n)
 	{
 		dest[i] = src[i];
-		i++
+		i++;
 	}
+
 	return (dest);
 }
