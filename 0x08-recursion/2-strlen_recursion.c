94859ac45f87208e8590ef57a9e7e58bcf8d60ce
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-06 02:22:13 +0000

    fix ?

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -10,10 +10,10 @@ int _strlen_recursion(char *s)
 {
 	unsigned int i =0;
 
-	if (*s !='\0')
+	if (*s != '\0')
 	{
-		i++;
-		return (_strlen_recursion(s+1));
+		++ i;
+		return (_strlen_recursion(s + 1));
 	}
 	else
 	{
