02ff152bfd0ded0bca3e850f5bfae334b7cdcb36
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-06 02:20:46 +0000

    third task first try

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the length of a string.
+ * @s: the string
+ * Return: the lenght of s
+*/
+
+int _strlen_recursion(char *s)
+{
+	unsigned int i =0;
+
+	if (*s !='\0')
+	{
+		i++;
+		return (_strlen_recursion(s+1));
+	}
+	else
+	{
+		return(i);
+	}
+}
