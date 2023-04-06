62aab019bcbb4bacb57155045d6eedb754320d06
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-06 01:30:12 +0000

    debug

--- 0x08-recursion/main.h
+++ 0x08-recursion/main.h
@@ -5,6 +5,10 @@
 /**
  * This is the header file
 */
+void _putchar(char c)
+{
+    putchar(c);
+}
 
 void _puts_recursion(char *s);
 
@@ -12,8 +16,4 @@ void _puts_recursion(char *s);
  * _putchar - putchar clone
  * @c: character to print
 */
-void _putchar(char c)
-{
-    putchar(c);
-}
 #endif
