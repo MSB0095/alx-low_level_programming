f887902421462c66325f259c0b46e4b6d87c23ed
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-07 01:19:08 +0000

    fix btty style

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- * _pow_recursion - a function that returns the value of x raised to the power of y.
+ * _pow_recursion - a function that returns the value of
+ *  x raised to the power of y.
  * @x: x
  * @y: y
- * Return: x raised tothe power of y 
+ * Return: x raised tothe power of y
 */
 
 int _pow_recursion(int x, int y)
@@ -14,7 +15,9 @@ int _pow_recursion(int x, int y)
 		return (-1);
 	}
 	else if (y != 0)
+	{
 		return (x * _pow_recursion(x, y - 1));
+	}
 	else
 	{
 		return (1);
