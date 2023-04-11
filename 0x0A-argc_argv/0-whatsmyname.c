94f94535c3a4810ecf23c7960e0fb3dc4c636931
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-11 01:50:34 +0000

    myname task

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+ * main - prints the program's path 
+ * @argc: arguments count
+ * @argv: arguments vector
+ * 
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,2 @@
+I am learning about argc and argv in C programming language
+This is part of my ALX software engineering program
