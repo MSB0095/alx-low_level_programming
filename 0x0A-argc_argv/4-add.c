1b5a1edcc18e696cb646559c1e72a12964d2479d
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-11 02:59:10 +0000

    last mandatory task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * main - multiplies the two parameters
+ * @argc: arguments count
+ * @argv: arguments vector
+ *
+ * Return: 0
+*/
+int main(int argc, char *argv[])
+{
+	if (argc != 3)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		int a, b, c;
+
+		a = atoi(argv[1]);
+		b = atoi(argv[2]);
+		c = a + b;
+		printf("%d\n", c);
+		return (0);
+
+	}
+}
