3bba245f658820297bc8d2d33b8348cf03fd263c
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-13 23:57:40 +0000

    first task, first try

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * *malloc_checked - a function that allocates memory using malloc.
+ * @b: number of bytes to allocate
+ * */
+
+void *malloc_checked(unsigned int b)
+{
+	malloc(b);
+}
---
+++ 0x0C-more_malloc_free/main.h
@@ -0,0 +1,9 @@
+#ifndef
+#define MAIN_H
+
+#include <stdlib.h>
+#include <stdio.h>
+
+void *malloc_checked(unsigned int b);
+
+#endif
