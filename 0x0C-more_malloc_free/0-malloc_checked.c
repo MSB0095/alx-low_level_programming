aafe519099bf2cb338dfb9abfd08dd1e60cb72a0
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-14 00:03:03 +0000

    fix

--- 0x0C-more_malloc_free/0-malloc_checked.c
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -3,9 +3,17 @@
 /**
  * *malloc_checked - a function that allocates memory using malloc.
  * @b: number of bytes to allocate
+ * Return: a pointer to the allocated memory
  * */
 
 void *malloc_checked(unsigned int b)
 {
-	malloc(b);
+	void *ptr;
+
+	ptr = malloc(b);
+
+	if (ptr == NULL)
+		exit(98);
+
+	return (ptr);
 }
