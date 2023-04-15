ae59f2d8dcec7cb8a6d080360ded24038baccc21
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-15 01:50:19 +0000

    calloc using memset

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,51 @@
+#include "main.h"
+
+/**
+ * _memset - function fill the first @n bytes of the memory area pointed
+ * to by @s with the constant byte @b
+ *
+ * @n: bytes of the memory area pointed to by @s
+ *
+ * @s: with the constant byte @b
+ *
+ * @b: memory area pointer
+ *
+ * Return: a pointer to the memory area @s
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+
+	while (i < n)
+	{
+	s[i] = b;
+	i++;
+	}
+
+	return (s);
+}
+
+/**
+ * _calloc - a function that allocates
+ *  memory for an array, using malloc
+ *
+ * @nmemb: number of elements
+ *
+ * @size: size of each element
+ *
+ * Return: a pointer to the allocated memory
+ * or NULL if it fails
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+		return (NULL);
+	_memset(ptr, 0, nmemb * size);
+	return (ptr);
+}
