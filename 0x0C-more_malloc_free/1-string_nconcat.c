0dff23e1b48eaec9d72911d625b63d2d930e4aad
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-15 01:34:14 +0000

    betty style fixes

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -11,37 +11,30 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i = 0, j = 0, len1 = 0,len2 = 0;
+	unsigned int i = 0, j = 0, len1 = 0, len2 = 0;
 	char *s;
-
 
 	while (s1[len1] != '\0')
 		len1++;
-
 	while (s2[len2] != '\0')
 		len2++;
-
 	if (s1 == NULL)
 		len1 = 0;
 	if (s2 == NULL)
 		len2 = 0;
-
 	if (n >= len2)
 		s = malloc(sizeof(char) * (len1 + len2 + 1));
 	else
 		s = malloc(sizeof(char) * (len1 + n + 1));
-
 	if (!s)
 	{
 		return (NULL);
 	}
-
 	while (i < len1)
 	{
 		s[i] = s1[i];
 		i++;
 	}
-
 	while (n < len2 && i < (len1 + n))
 	{
 		s[i++] = s2[j++];
@@ -50,8 +43,6 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		s[i++] = s2[j++];
 	}
-
 	s[i] = '\0';
-
 	return (s);
 }
