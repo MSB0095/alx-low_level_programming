0423d81b7fdaa3da86ce11aa9c2a664a04f20279
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-19 00:59:21 +0000

    fix ?

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -10,7 +10,12 @@
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	if (d == NULL)
+		return;
+	else
+	{
+		d->name = name;
+		d->age = age;
+		d->owner = owner;
+	}
 }
