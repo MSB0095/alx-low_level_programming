8b7fcc7009915803637d7b03b568104b1eaa3820
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-19 00:47:29 +0000

    fixes

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,12 +1,19 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - a structure that contains name age owner
+ * @name: the name of the dog
+ * @age: the age of the dog
+ * @owner: dog's owner
+ *
+ * Description: a simple struct
+*/
 struct dog
 {
 	char *name;
 	float age;
 	char *owner;
-	/* name , age , owner */
-};
+}
 
 #endif
