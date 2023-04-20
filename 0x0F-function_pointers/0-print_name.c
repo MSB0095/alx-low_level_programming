c5cbd8ace84745b6a9fcffc44bcf992eff0527d4
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-20 01:59:09 +0000

    print_name

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * print_name - a function that prints a name.
+ * @name: the name to print
+ * @f: the function pointer to the function that prints
+*/
+
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
