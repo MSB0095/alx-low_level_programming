49bacdaf14dfe43d2c9ccc2e730f068130be6f34
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-20 23:48:00 +0000

    int_index

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,23 @@
+#include "function_pointers.h"
+
+/**
+ * int_index - a function that searches for an integer.
+ * @array: the array to search in
+ * @size: the size of the array
+ * @cmp: the function that checks the integer
+ * Return: -1 if size <= O or no int found , else the int 
+*/
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+			return (array[i]);
+	}
+
+	return (-1);
+}
