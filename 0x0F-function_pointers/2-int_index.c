37f8960c92d4f4298335224dbb17e3598f9a8035
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-20 23:49:40 +0000

    fixes

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -5,13 +5,15 @@
  * @array: the array to search in
  * @size: the size of the array
  * @cmp: the function that checks the integer
- * Return: -1 if size <= O or no int found , else the int 
+ * Return: -1 if size <= O or no int found , else the int
 */
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
 	if (size <= 0)
 		return (-1);
+
+	unsigned int i = 0;
 
 	for (i = 0; i < size; i++)
 	{
