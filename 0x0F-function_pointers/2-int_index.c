ea8894aefbd81adf6975bb7c17ce2da3a8fd9ab4
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-21 00:03:22 +0000

    add condition that array and cmp are not NULL

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -15,10 +15,13 @@ int int_index(int *array, int size, int (*cmp)(int))
 
 	unsigned int i = 0;
 
-	for (i = 0; i < size; i++)
+	if (array && cmp)
 	{
-		if (cmp(array[i]) != 0)
-			return (array[i]);
+		for (i = 0; i < size; i++)
+		{
+			if (cmp(array[i]) != 0)
+				return (array[i]);
+		}
 	}
 
 	return (-1);
