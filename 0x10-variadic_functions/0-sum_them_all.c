b43b5bbd9a6e39fa5f72874e203873c72f29cf34
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-21 02:36:47 +0000

    fix

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,7 +1,8 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - a function that returns the sum of all its parameters
+ * sum_them_all - a function that returns the sum of all
+ *  its parameters
  * @n: number of parameters
  * Return: the sum or 0 if n == 0
 */
