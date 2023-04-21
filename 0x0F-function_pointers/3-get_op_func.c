d591ba53a63adce0f86aa04272bf71a2f5d63391
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-21 01:30:06 +0000

    last task

---
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -0,0 +1,31 @@
+#include "3-calc.h"
+
+/**
+ * get_op_func - a function that selects the operation
+ * @s: the entered operation
+ * Return: a pointer to the corresponding operation function
+*/
+
+int (*get_op_func(char *s))(int, int)
+{
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i = 0;
+
+	while (ops[i].op)
+	{
+		if (strcmp(ops[i].op, s) == 0)
+		{
+			return (ops[i].f);
+		}
+		i++;
+	}
+
+	return (NULL);
+}
---
+++ 0x0F-function_pointers/3-op_functions.c
@@ -0,0 +1,62 @@
+#include "3-calc.h"
+
+/**
+ * op_add - a function that adds 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: a+b
+*/
+
+int op_add(int a, int b)
+{
+	return(a + b);
+}
+
+/**
+ * op_sub - a function that substracts 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: a-b
+*/
+
+int op_sub(int a, int b)
+{
+	return(a - b);
+}
+
+/**
+ * op_mul - a function that multiplies 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: a*b
+*/
+
+int op_mul(int a, int b)
+{
+	return(a * b);
+}
+
+/**
+ * op_div - a function that divides 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: a/b
+*/
+
+int op_div(int a, int b)
+{
+	return(a / b);
+}
+
+/**
+ * op_mod - a function that modulo 2 integers
+ * @a: first int
+ * @b: second int
+ * Return: a%b
+*/
+
+int op_mod(int a, int b)
+{
+	return(a % b);
+}
+
