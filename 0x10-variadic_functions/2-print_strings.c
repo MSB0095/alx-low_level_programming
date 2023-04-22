45d3e82bcf5ec1befcb72ae60ad68a2eeaee2d83
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-22 00:37:56 +0000

    fixes

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,11 +14,11 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	for (i = 0; i < n; i++)
 	{
 		if (!separator)
-			printf("%d", va_arg(va, char *));
+			printf("%s", va_arg(va, char *));
 		else if (separator && i == 0)
-			printf("%d", va_arg(va, char *));
+			printf("%s", va_arg(va, char *));
 		else
-			printf("%s%d", separator, va_arg(va, char *));
+			printf("%s%s", separator, va_arg(va, char *));
 	}
 	va_end(va);
 	printf("\n");
