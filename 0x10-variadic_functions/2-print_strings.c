97e8a9c12af0694e5d2f52f23b43123387654980
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-22 00:41:45 +0000

    fix

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -13,7 +13,7 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(va, n);
 	for (i = 0; i < n; i++)
 	{
-		if (!va_arg(va, char *))
+		if (va_arg(va, char *) == NULL)
 		{
 			printf("(nil)");
 			break;
