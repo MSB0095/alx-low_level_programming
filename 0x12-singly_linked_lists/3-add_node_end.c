59fe0f74a655f614c2c4ff60ded1e363f3e4c191
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-04-29 01:24:52 +0100

    fix betty

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -1,7 +1,7 @@
 #include "lists.h"
 
 /**
- * add_node - adds a new node at the beginning of a list_t list.
+ * add_node_end - adds a new node at the end of a list_t list.
  * @head : double pointer to the list_t list
  * @str: new string to add
  *
