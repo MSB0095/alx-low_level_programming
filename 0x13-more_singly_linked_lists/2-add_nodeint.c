08bf79bbe14f9c325020319e5d5e58bfa949ceaf
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-05-01 23:59:33 +0100

    retry ?

--- 0x13-more_singly_linked_lists/2-add_nodeint.c
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -15,8 +15,9 @@ listint_t *add_nodeint(listint_t **head, const int n)
 {
 	listint_t *new;
 
+	new = malloc(sizeof(listint_t));
 	new->n = n;
-	new->next = head;
+	new->next = *head;
 
 	return (new);
 
