8b29f7e182ec0537c610dd1a86d5b9dfd40dc3b9
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-05-03 00:59:57 +0100

    fix

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -14,11 +14,12 @@ int pop_listint(listint_t **head)
 	listint_t *temp;
 	int n;
 
-	if (!head)
+	if (!head || !*head)
 		return (0);
 	n = (*head)->n;
 
 	temp = (*head)->next;
+	free(*head);
 	*head = temp;
 
 	return (n);
