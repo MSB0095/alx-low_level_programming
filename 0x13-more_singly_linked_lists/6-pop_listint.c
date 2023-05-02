8b564caddbf2c09914dd6955b178f9a0b4930ddc
MSB0095 <125674876+MSB0095@users.noreply.github.com>
2023-05-03 00:58:20 +0100

    remove head of a list

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,25 @@
+#include "lists.h"
+
+/**
+ * pop_listint - a function that deletes the head node
+ * of a listint_t linked list,
+ *
+ * @head: the head
+ *
+ * Return: the head node’s data (n).
+*/
+
+int pop_listint(listint_t **head)
+{
+	listint_t *temp;
+	int n;
+
+	if (!head)
+		return (0);
+	n = (*head)->n;
+
+	temp = (*head)->next;
+	*head = temp;
+
+	return (n);
+}
