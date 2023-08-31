5c0ff927f07185b17d3d599acd2ea28b07726b3c
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-09-01 00:51:26 +0100

    betty fixes

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,32 +8,32 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int n = 0;
-    unsigned int l = _strlen(b);
-    int p = 1;
+	unsigned int n = 0;
+	unsigned int l = _strlen(b);
+	int p = 1;
 
-    if (!b)
-    {
-        return (0);
-    }
-    else
-    {
-        while (l > 0)
-        {
-            if (b[l - 1] == '1' || b[l - 1] == '0')
-            {
-                if (b[l - 1] == '1')
-                {
-                    n = n + p;
-                }
-                p = p * 2;
-                l--;
-            }
-            else
-            {
-                return (0);
-            }
-        }
-        return (n);
-    }
+	if (!b)
+	{
+		return (0);
+	}
+	else
+	{
+		while (l > 0)
+		{
+			if (b[l - 1] == '1' || b[l - 1] == '0')
+			{
+				if (b[l - 1] == '1')
+				{
+					n = n + p;
+				}
+				p = p * 2;
+				l--;
+			}
+			else
+			{
+				return (0);
+			}
+		}
+		return (n);
+	}
 }
--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -1,2 +1,7 @@
+#ifndef MAIN_H
+#define MAIN_H
+
 unsigned int _strlen(const char *s);
 unsigned int binary_to_uint(const char *b);
+
+#endif
