9bc2d1b4641fed88f6c8ed4df8bdbb30fd1be125
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-09-09 00:04:23 +0100

    create_file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -1,1 +1,29 @@
-#include
+#include "main.h"
+
+/**
+ * create_file - a function that creates a file
+ * @filename: the name of the file to create
+ * @text_content: text content to write
+ * Return: 1 on success -1 on failure
+*/
+
+int create_file(const char *filename, char *text_content)
+{
+    int o, w, len;
+
+    if (!filename)
+        return (-1);
+    if (text_content)
+    {
+        for (len = 0; text_content[len];)
+        {
+            len++;
+        }
+    }
+    o = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600 );
+    w = write(o, text_content, len);
+    if (o == -1 || w == -1)
+        return (-1);
+    close(o);
+    return (1);
+}
