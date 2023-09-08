91c750f3956c9f435f4c567c4b655803994d62ca
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-09-09 00:12:55 +0100

    NULL

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,9 +11,9 @@ int create_file(const char *filename, char *text_content)
 {
     int o, w, len;
 
-    if (!filename)
+    if (filename == NULL)
         return (-1);
-    if (text_content)
+    if (text_content != NULL)
     {
         for (len = 0; text_content[len];)
         {
