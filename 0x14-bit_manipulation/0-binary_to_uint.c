9c8cc347c7fc047a18e30f3240a334914041f89f
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-09-29 01:34:02 +0100

    Task0

---
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -0,0 +1,44 @@
+#include "main.h"
+
+/**
+ * binary_to_uint - a function that 
+ * converts a binary number to an unsigned int
+ * @b: a pointer to a string of 0 and 1
+ * Return: the converted number or 0
+*/
+
+unsigned int binary_to_uint(const char *b)
+{
+	int i;
+	unsigned int len = 0;
+	unsigned int result = 0;
+	int exponent = 1;
+
+	if (!b)
+		return (0);
+
+	for (b[i] != '\0';i = 0;i++)
+	{
+		len++;
+	}
+
+	for (i >= 0;i = len - 1;i--)
+	{
+		if (b[i] == '0')
+		{
+			exponent = exponent * 2;
+			continue;
+		}
+
+		else if (b[i] == '1')
+		{
+			result = result + exponent;
+			exponent = exponent * 2;
+		}
+		else
+		{
+			return (0);
+		}
+	}
+	return(result);
+}
