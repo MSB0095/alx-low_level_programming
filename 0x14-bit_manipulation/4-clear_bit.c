24da1e6ffd346351dd27e5f733de521cca20d686
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-09-29 21:31:17 +0100

    clear_bit that was easy , just modifying set_bit

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -14,17 +14,17 @@ int clear_bit(unsigned long int *n, unsigned int index)
 	unsigned long int mask;
 	unsigned long int pow;
 
-	if (index >= sizeof(unsigned long int) * 8)
+	if ((index >= sizeof(unsigned long int) * 8) | !n)
 		return (-1);
 
 	mask = (*n >> index);
 
-	if ((mask & 1) == 1)
+	if ((mask & 1) == 0)
 		return (1);
-	else if ((mask & 1) == 0)
+	else if ((mask & 1) == 1)
 	{
 		pow = 1 << index;
-		*n = *n + pow;
+		*n = *n - pow;
 		return (1);
 	}
 	else
