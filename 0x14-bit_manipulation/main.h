d26f403893caa21b50edefc97ba477f1b68eb005
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-09-29 21:25:46 +0100

    add !n case

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -6,5 +6,6 @@ void print_binary(unsigned long int n);
 void _putchar(unsigned long int c);
 int get_bit(unsigned long int n, unsigned int index);
 int set_bit(unsigned long int *n, unsigned int index);
+int clear_bit(unsigned long int *n, unsigned int index);
 
 #endif
