43c9a19c261c380d5ff42a0270ec590467690592
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-10-02 23:33:53 +0100

    fix betty and other issues

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -14,11 +14,14 @@ int create_file(const char *filename, char *text_content)
 	if (filename == NULL)
 		return (-1);
 	if (text_content != NULL)
-		for (i = 0; text_content[i] != '\0'; i++);
+	{
+		for (i = 0; text_content[i] != '\0'; i++)
+			;
+	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, i);
-	if (op == -1 || wr == -1 )
+	if (op == -1 || wr == -1)
 		return (0);
 
 	close(op);
