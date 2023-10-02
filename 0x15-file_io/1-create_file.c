b96fe804308e36cef224e1bd578fb5c79f15ac68
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-10-02 23:39:03 +0100

    fix stupid error : return (0)

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -22,7 +22,7 @@ int create_file(const char *filename, char *text_content)
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
 	wr = write(op, text_content, i);
 	if (op == -1 || wr == -1)
-		return (0);
+		return (-1);
 
 	close(op);
 	return (1);
