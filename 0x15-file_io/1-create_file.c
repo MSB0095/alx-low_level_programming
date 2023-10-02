eb8642cec073e116a83cc552cc40f02083864dce
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-10-02 23:35:46 +0100

    fix ?

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -15,8 +15,8 @@ int create_file(const char *filename, char *text_content)
 		return (-1);
 	if (text_content != NULL)
 	{
-		for (i = 0; text_content[i]; i++)
-			;
+		for (i = 0; text_content[i];)
+			i++;
 	}
 
 	op = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
