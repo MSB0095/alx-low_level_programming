d90961edea16a2cf7911bdd8061b0344d8adcd22
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-10-03 00:01:53 +0100

    first commit

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * main - main function of a program that copies
+ * a file to another file
+ * @ac: arguments count
+ * @av: arguments vector
+ * Return: 0
+*/
+
+int main (int ac, char **av)
+{
+	if (ac != 3)
+	{
+		dprintf("Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	
+}
