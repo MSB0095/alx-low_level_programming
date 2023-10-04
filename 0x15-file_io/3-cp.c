140a11bd7b4bfc2c731d99b4af25f897f07001cb
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-10-04 22:09:11 +0100

    last fixes , i am exhausted

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,7 +19,7 @@ int main(int ac, char **av)
 		dprintf(2, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	ffrom = open(av[1],O_RDONLY);
+	ffrom = open(av[1], O_RDONLY);
 	if (ffrom == -1)
 	{
 		dprintf(2, "Error: Can't read from file %s\n", av[1]);
@@ -28,7 +28,8 @@ int main(int ac, char **av)
 	fto = open(av[2], O_WRONLY | O_CREAT | O_TRUNC);
 	if (fto == -1)
 	{
-
+		dprintf(2, "Error: Can't write to %s\n", av[2]);
+		close(ffrom);
 		exit(99);
 	}
 	while ((rd = read(ffrom, buf, BUFSIZE)) > 0)
@@ -36,12 +37,13 @@ int main(int ac, char **av)
 		if (write(fto, buf, rd) != rd)
 		{
 			dprintf(2, "Error: Can't write to %s\n", av[2]);
+			close(ffrom);
 			exit(99);
 		}
 	}
 	c1 = close(ffrom);
 	c2 = close(fto);
-	if (c1 == -1 )
+	if (c1 == -1)
 	{
 		dprintf(2, "Error: Can't close fd %d\n", ffrom);
 		exit(100);
