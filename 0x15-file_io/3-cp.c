fac228837af243e7b51f1ceab0f523883b9243af
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2023-10-04 21:13:43 +0100

    added some code

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -10,9 +10,23 @@
 
 int main(int ac, char **av)
 {
+	char *buf;
+	int op, rd, wr;
+
 	if (ac != 3)
 	{
 		dprintf("Usage: cp file_from file_to\n");
 		exit(97);
+	}
+	op = open(av[1],O_RDONLY);
+	if (op == -1)
+	{
+		dprintf("Error: Can't read from file %s\n", av[1]);
+		exit(98);
+	}
+	rd = read(op, buf, BUFSIZE);
+	if (rd == -1)
+	{
+
 	}
 }
