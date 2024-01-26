91848a115cbf47453801182693074a938c34cec9
MSB0095 <andromedianmsb01@gmail.com>
2024-01-26 01:26:35 +0100

    Add free_dlistint function to free a dlistint_t list

---
+++ 0x17-doubly_linked_lists/4-free_dlistint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * free_dlistint - frees a dlistint_t list
+ *
+ * @head: pointer to head of list
+ *
+ * Return: void
+ */
+
+void free_dlistint(dlistint_t *head)
+{
+	dlistint_t *temp;
+
+	if (head)
+	{
+		while (head != NULL)
+		{
+			temp = head;
+			head = head->next;
+			free(temp);
+		}
+	}
+}
