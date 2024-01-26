eb2d1a0a87550f964d146c597ea8ea46aa980d28
MSB0095 <andromedianmsb01@gmail.com>
2024-01-26 01:31:28 +0100

    Add get_dnodeint_at_index function to retrieve nth node from a doubly linked list

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ *
+ * @head: pointer to head of list
+ * @index: index of node to return
+ *
+ * Return: address of nth node or NULL if failed
+ */
+
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+    unsigned int counter = 0;
+
+    if (head == NULL)
+        return (NULL);
+
+    while (head != NULL)
+    {
+        if (counter == index)
+            return (head);
+
+        head = head->next;
+        counter++;
+    }
+
+    return (NULL);
+}
