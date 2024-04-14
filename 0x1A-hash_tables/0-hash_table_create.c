bd2409e032befa5aa605cf8926198a8c4007c347
MSB0095 <andromedianmsb01@gmail.com>
2024-04-15 00:18:20 +0100

    add hash_table_create

---
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+/**
+ * hash_table_create - creates and inits a hash table
+ * @size: the size
+ * Return: the table or NULL if failed
+*/
+
+hash_table_t *hash_table_create(unsigned long int size)
+{
+	long unsigned int i;
+	hash_table_t *table;
+	hash_node_t **array;
+
+	table = malloc(sizeof(hash_table_t));
+	if (table == NULL)
+	{
+		return (NULL);
+	}
+	array = malloc(size * sizeof(hash_node_t *));
+	if (array == NULL)
+	{
+		free(table);
+		return (NULL);
+	}
+	table->size = size;
+	table->array = array;
+	for (i = 0; i < size; i++)
+	{
+		table->array[i] = NULL;
+	}
+	return (table);
+}
