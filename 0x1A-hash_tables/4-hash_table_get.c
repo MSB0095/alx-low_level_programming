8d82e5111bbfd0dc4d5c3e0f6f7cb6fc119e2a11
MSB0095 <mohamedsalah.belbsir2@gmail.com>
2024-08-08 18:29:50 +0100

    feat: Implement hash_table_get function for retrieving values from the hash table

---
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -0,0 +1,32 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * hash_table_get - Retrieves a value associated with a key.
+ * @ht: The hash table to look into.
+ * @key: The key to look for.
+ *
+ * Return: The value associated with the element,
+ * or NULL if key couldn't be found.
+ */
+char *hash_table_get(const hash_table_t *ht, const char *key)
+{
+	unsigned long int index;
+	hash_node_t *current_node;
+
+	if (!ht || !key || !*key)
+		return (NULL);
+
+	index = key_index((const unsigned char *)key, ht->size);
+	current_node = ht->array[index];
+
+	while (current_node)
+	{
+		if (strcmp(current_node->key, key) == 0)
+			return (current_node->value);
+		current_node = current_node->next;
+	}
+
+	return (NULL);
+}
